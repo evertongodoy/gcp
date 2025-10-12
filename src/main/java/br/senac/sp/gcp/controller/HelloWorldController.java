92d3806d869d4a3f9c6f639753917759f4931fbd
Everton Godoy <everton.mgodoy@gmail.com>
2025-10-12 14:30:55 -0300

    first commit

---
+++ src/main/java/br/senac/sp/gcp/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package br.senac.sp.gcp.controller;
+
+import br.senac.sp.gcp.controller.response.HelloWorldResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("gcp")
+public class HelloWorldController {
+
+    @GetMapping("hello-world")
+    public ResponseEntity<HelloWorldResponse> helloWorld() {
+        return ResponseEntity.ok().body(
+                new HelloWorldResponse("Hello, World!")
+        );
+    }
+
+}
