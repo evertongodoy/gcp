9b16589df3f133ef5cbbcd25e5148e94ccf2d8ac
Everton Godoy <everton.mgodoy@gmail.com>
2025-10-20 21:49:53 -0300

    recuperado entidade

--- src/main/java/br/senac/sp/gcp/controller/HelloWorldController.java
+++ src/main/java/br/senac/sp/gcp/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import br.senac.sp.gcp.controller.request.MensagemRequest;
 import br.senac.sp.gcp.controller.response.HelloWorldResponse;
 import br.senac.sp.gcp.controller.response.MensagemResponse;
 import br.senac.sp.gcp.database.Mensagem;
+import br.senac.sp.gcp.dtos.MensagemDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,8 +25,8 @@ public class HelloWorldController {
 
     @PostMapping("salvar-mensagem")
     public ResponseEntity<MensagemResponse> cadastrarMensagem(@RequestBody MensagemRequest request){
-        var dto = mensagem.salvarMensagem(request.toDTO());
-        return ResponseEntity.ok().body(dto.toResponse());
+//        var dto = mensagem.salvarMensagem(request.toDTO());
+        return ResponseEntity.ok().body(MensagemDTO.builder().build().toResponse());
     }
 
 }
--- src/main/java/br/senac/sp/gcp/database/repository/MensagemRepository.java
+++ src/main/java/br/senac/sp/gcp/database/repository/MensagemRepository.java
@@ -1,10 +1,10 @@
-package br.senac.sp.gcp.database.repository;
-
-import br.senac.sp.gcp.database.entities.MensagemEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MensagemRepository extends JpaRepository<MensagemEntity, Long> {
-
-}
+//package br.senac.sp.gcp.database.repository;
+//
+//import br.senac.sp.gcp.database.entities.MensagemEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface MensagemRepository extends JpaRepository<MensagemEntity, Long> {
+//
+//}
