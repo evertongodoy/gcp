8d0f4f5dc74feed9f5d9d0b2d7aea15ec1d731e8
Everton Godoy <everton.mgodoy@gmail.com>
2025-10-20 22:12:50 -0300

    resetando branch aula

--- src/main/java/br/senac/sp/gcp/controller/HelloWorldController.java
+++ src/main/java/br/senac/sp/gcp/controller/HelloWorldController.java
@@ -1,9 +1,6 @@
 package br.senac.sp.gcp.controller;
 
-import br.senac.sp.gcp.controller.request.MensagemRequest;
 import br.senac.sp.gcp.controller.response.HelloWorldResponse;
-import br.senac.sp.gcp.controller.response.MensagemResponse;
-import br.senac.sp.gcp.database.Mensagem;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,19 +10,11 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class HelloWorldController {
 
-    final Mensagem mensagem;
-
     @GetMapping("hello-world")
     public ResponseEntity<HelloWorldResponse> helloWorld() {
         return ResponseEntity.ok().body(
                 new HelloWorldResponse("Hello, World, atualizado!!")
         );
-    }
-
-    @PostMapping("salvar-mensagem")
-    public ResponseEntity<MensagemResponse> cadastrarMensagem(@RequestBody MensagemRequest request){
-        var dto = mensagem.salvarMensagem(request.toDTO());
-        return ResponseEntity.ok().body(dto.toResponse());
     }
 
 }
